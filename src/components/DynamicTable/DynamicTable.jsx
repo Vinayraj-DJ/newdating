d6365e711addaf725b9ec0f18e621eaf2462ea75
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2026-01-20 18:23:39 +0530

    third commit

--- src/components/DynamicTable/DynamicTable.jsx
+++ src/components/DynamicTable/DynamicTable.jsx
@@ -9,7 +9,7 @@ function DynamicTable({ headings = [], columnData = [], noDataMessage }) {
         <thead>
           <tr>
             {headings.map((heading, index) => (
-              <th key={index}>{heading.title}</th>
+              <td key={index}>{heading.title}</td>
             ))}
           </tr>
         </thead>
